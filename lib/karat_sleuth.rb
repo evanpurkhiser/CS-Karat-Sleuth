aad3f4e278d5d7024424186d5898742b4cd0b56e
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-12-02 15:59:45 -0500

    Basic library API
    This is a semi-implemented outline of how I _think_ the static API for
    KaratSeluth should work. You deal with Email objects. The two primary
    actions that can be taken is to train KaratSleuth from a message or
    classify a message

--- lib/karat_sleuth.rb
+++ lib/karat_sleuth.rb
@@ -1,6 +1,53 @@
 require "karat_sleuth/version"
 require "karat_sleuth/storage"
 
+require 'mail'
+
 module KaratSleuth
 
+	# These are the two valid classifications for email
+	CLASSIFICATIONS = [:ham, :spam]
+
+	# Convert a email file into a message object
+	def path_to_message(path)
+		Mail.read(path)
+	end
+
+	# Convert a string to a message object
+	def string_to_message(string)
+		Mail.read_from_string(string)
+	end
+
+	# Learn from a message
+	def train_from_message(message, classification)
+		raise "Invalid message classification" unless CLASSIFICATIONS.include? classification
+
+		# Do 'something' to train the classifier from this message
+	end
+
+	# Classify a message
+	def classify_message(message)
+		message.classify
+	end
+
+	# Extend the Mail class to support various classification methods
+	module MailClassifiers
+
+		# Determine the classification of this message
+		def classify
+			:ham
+		end
+
+		# Test if a given message is classified has ham
+		def is_ham?
+			true
+		end
+
+		# Test if a given message is classified as spam
+		def is_spam?
+			false
+		end
+	end
+
+	Mail.send(:include, MailClassifiers)
 end
